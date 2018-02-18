5aed1f58cf676a1976ea446067a0c494df063aa8
AlejandroVC67 <vcalejandro97@gmail.com>
2018-02-17 19:23:58 -0500

    updating states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'This is the state',
+      count: '0'
+    }
+  }
+  update (event){
+    this.setState({txt: event.target.value})
+  }
+  render (){
+    return (
+      <div> 
+        <input type='text' onChange={this.update.bind(this)} />
+        <h1 className="Title">{this.state.txt} - {this.state.count}</h1> 
+      </div>
+    )
+  }
+}
 
 export default App
